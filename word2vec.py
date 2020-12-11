af90c6c067a7318b5d58efb260dc4728d9d25ccc
Smruthi Ramesh <rameshsmruthi@gmail.com>
2020-12-10 20:10:32 -0500

    basic word2vec embeddings for train and dev

--- word2vec.py
+++ word2vec.py
@@ -3,37 +3,71 @@
 import numpy as np
 from utils import read_records, adv_tokenizer, encode_labels
 from gensim.models import Word2Vec
-import scispacy
-import en_core_sci_md
+from nltk.corpus import stopwords
+from nltk.stem.snowball import SnowballStemmer
 from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeClassifier
-nlp = en_core_sci_md.load()
-
-# from spacy.lang.en import English
-# en = English()
-
-#loading train records
-train_records = read_records('train/')
-train_text = [train_records[x]['text'] for x in train_records]
-train_tags = [train_records[x]['tags'] for x in train_records]
-
-#list of unique criteria
-criteria = ['ABDOMINAL', 'ADVANCED-CAD', 'ASP-FOR-MI', 'ENGLISH', 'MAKES-DECISIONS', 'ALCOHOL-ABUSE', 'DIETSUPP-2MOS', 'CREATININE', 'HBA1C', 'MAJOR-DIABETES', 'MI-6MOS', 'DRUG-ABUSE', 'KETO-1YR']
-
-#encoding tags with unique labels 
-train_labels = encode_labels(train_tags, criteria)
+from sklearn.metrics import accuracy_score,roc_auc_score, precision_recall_fscore_support
+import json
+import matplotlib.pyplot as plt
+#loading stopwords
+stopwords = set(stopwords.words('english'))
+stemmer = SnowballStemmer("english")
 
 #tokenizing text using scispacy
-train_text_list = []
-for text in train_text:
-    #only adding valid scispacy entities
-    doc = nlp(text)
-    train_text_list.append([x.text for x in doc.ents])
+def tokenize(doc):
+    text_list = []
+    for text in doc:
+        #only adding valid scispacy entities - lowercase, stemmed, stopwords removed
+        tokens = text.split(' ')
+        text_list.append([x.lower() for x in tokens if x not in stopwords])
+    return text_list
+
+#gets wv representation of a sentence
+def sent_to_wv(X):
+    X_new = []
+    for record in X:
+        sent_list = []
+        for word in record:
+            try:
+                sent_list.append(model.wv.get_vector(word))
+            except:
+                pass            
+        sent_array = np.mean(np.array(sent_list), axis=0)
+        X_new.append(sent_array)
+    return X_new
+
+def process_text(text_dict,criteria)
+    train_text = [train_records[x]['text'] for x in text_dict]
+    train_tags = [train_records[x]['tags'] for x in text_dict]
+
+    #encoding tags with unique labels 
+    train_labels,label_mappings = encode_labels(train_tags, criteria)
+
+    X_train = tokenize(train_text)
+
+    mlb = MultiLabelBinarizer()
+    y_train = mlb.fit_transform(train_labels)
+
+    return X_train,y_train
 
 
-#splitting into train and dev sets for testing purposes
-X_train, X_dev, y_train, y_dev = train_test_split() 
+def word2vec(criteria, train_file, dev_file):  
+    #loading train records
+    with open(train_file,'r') as train:
+        train_records = json.load(train)
+    #loading dev1 records
+    with open(dev_file,'r') as dev:
+        dev_records = json.load(dev)
+    #adding dev1 to train for word embeddings purpose
+    train_records.update(dev_records)    
+    X_train, y_train = process_text(train_records)
+    #training word vectors on dataset
+    model = Word2Vec(X_train, min_count=5,size=100,workers=3, window=5, sg = 1)
+    #getting representation for train
+    X_train_word2vec = np.array(sent_to_wv(X_train))
+    return X_train_word2vec
 
-#training word vectors on dataset
-model = Word2Vec(train_text_list, min_count=1,size= 50,workers=3, window =3, sg = 1)
 
+#narrowed down criteria
+criteria = ['ABDOMINAL', 'ADVANCED-CAD', 'ASP-FOR-MI', 'DIETSUPP-2MOS', 'CREATININE', 'MAJOR-DIABETES']
+embeddings = word2vec(criteria,'./data/train.txt','./data/dev.txt')
